47e23afed9d604e57da8b925a0f269eaf8861857
Wiktor Wójcik <wiktor@studiowojcik.eu>
2023-05-09 12:09:26 +0200

    zmian

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const btn = document.querySelector('header a');
+
+const h1 = document.querySelector('header h1');
+
+const  h1Text = h1.innerText;
+
+let counter = 0;
+
+btn.addEventListener('click', () => {
+    counter++
+
+    if(counter % 2 === 0)
+        h1.innerText = h1Text;
+    else
+        h1.innerText = 'Będzie Pan zadowolony :)'
+
+});
